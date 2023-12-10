4f7c5f16e8f2da60e2ee3a6df5535021fda0c610
zaiaaa <gustavo.0801214@gmail.com>
2023-12-10 19:15:08 -0300

    finish

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.use(cors({
 }))
 
 
-app.listen(process.env.PORT || 3000, (error) => {
+app.listen(process.env.PORT || 3001, (error) => {
     if(error){
         console.log('deu erro -> ', error.message)
     }else{
-        console.log(`rodando na porta ${process.env.PORT || 3000}`)
+        console.log(`rodando na porta ${process.env.PORT || 3001}`)
     }
 })
